aa1829fc00983747514c98791857d5ae0f6addf5
Gena709 <alexeu989@gmail.com>
2023-01-30 20:34:03 +0500

    five commit

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, current_app, url_for, redirect, request, flash
 
-from sqlalchemy import create_engine, Column, String, INTEGER, Date
-
-from sqlalchemy.orm import declarative_base, sessionmaker
-
 from datetime import datetime
+
+from database import Student_base, Session
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
@@ -13,39 +11,7 @@ with app.app_context(): # создание контекста
     # within this block, current_app points to app.
     print(current_app.name)
 
-
-#----------------------------СОЗДАНИЕ БАЗЫ ДАННЫХ-----------------------------------
-engine = create_engine('sqlite:///instance/student_base.db?check_same_thread=False', echo=True)
-
-Base = declarative_base()
-
-class Student_base(Base):
-    __tablename__ = "Students" # имя таблицы
-
-    id = Column("id", INTEGER, primary_key=True)
-    name = Column("Name", String, nullable=False) # string приравнивается к TEXT
-    surname = Column("Surname", String, nullable=False)
-    dateofbirth = Column("Date", Date, nullable=False)
-    phonenumber = Column("Phone_number", String, nullable=False, unique=True) # проверить длину изменить ьип данных
-    email = Column("email", String, nullable=False, unique=True)
-
-    def __init__(self, name, surname, dateofbirth, phonenumber, email): # контсруктор класса (перегрузка создания экземпляра)
-
-        self.name = name
-        self.surname = surname
-        self.dateofbirth = dateofbirth
-        self.phonenumber = phonenumber
-        self.email = email
-
-    def __repr__(self):# магический метод для отображения информации об объекте класса в режиме отладки
-        return f"{self.__class__}: {self.name}, {self.surname}, {self.dateofbirth}, {self.phonenumber}, {self.email}"
-
-Base.metadata.create_all(bind=engine)
-
-Session = sessionmaker(bind=engine) # соединение с бд
 session = Session()
-#-------------------------------------------------------------------------------------
-
 
 #----------------------------ПРОВЕРКА ДАННЫХ ИЗ ВЕБ-ФОРМ В БД-------------------------------
 def check_data(phonenumber, email, id=None): # проверяю только номер телефона  и почту, так как остальные поля могут быть идентичнымит
